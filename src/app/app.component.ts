e8e28a013e5164cea91384b899684af276729caa
Flavio Santos <flavio.a.dos.santos@avanade.com>
2025-04-10 14:27:01 -0300

    feat: Header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./components/template/header/header.component";
+import { FooterComponent } from "./components/template/footer/footer.component";
+import { NavComponent } from './components/template/nav/nav.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
-  `,
-  styles: [],
+  templateUrl: './app.component.html',
+  imports: [HeaderComponent, FooterComponent, NavComponent, MatToolbarModule],  
 })
-export class AppComponent {
-  title = 'frontend';
+
+export class AppComponent {  
+  
 }
