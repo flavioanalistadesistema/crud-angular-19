637261a8a8accd659dd6d3f22a422f71442c30c3
Flavio Santos <flavio.a.dos.santos@avanade.com>
2025-04-12 19:53:32 -0300

    feat: Config Router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,11 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "./components/template/header/header.component";
 import { FooterComponent } from "./components/template/footer/footer.component";
 import { NavComponent } from './components/template/nav/nav.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, FooterComponent, NavComponent, MatToolbarModule],  
+  imports: [HeaderComponent, FooterComponent, NavComponent],  
 })
 
 export class AppComponent {  
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { ProductComponent } from './views/product/product.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [    
+{ path: '', component: HomeComponent },
+{ path: 'products', component: ProductComponent },
+{ path: '**', redirectTo: '' } 
+];
