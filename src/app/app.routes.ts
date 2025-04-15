f6e62f8cb0f80f4bd1885734393b6911a5cd648f
Flavio Santos <flavio.a.dos.santos@avanade.com>
2025-04-15 11:49:29 -0300

    feat: Incluindo novo component Router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
 import { ProductComponent } from './views/product/product.component';
+import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 
 export const routes: Routes = [    
 { path: '', component: HomeComponent },
 { path: 'products', component: ProductComponent },
+{ path: 'products/createad', component: ProductCreateComponent },
 { path: '**', redirectTo: '' } 
 ];
