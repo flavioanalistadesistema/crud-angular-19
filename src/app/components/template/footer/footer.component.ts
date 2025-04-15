2b0840b13e551dc0747b95de6dadc0a1d33423cd
Flavio Santos <flavio.a.dos.santos@avanade.com>
2025-04-15 11:52:54 -0300

    feat: incluindo diretiva de atributo no componente footer

--- src/app/components/template/footer/footer.component.ts
+++ src/app/components/template/footer/footer.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { RedDirective } from '../../../Directive/red.redirective';
 
 @Component({
   selector: 'app-footer',
-  imports: [MatToolbarModule],
+  imports: [MatToolbarModule, RedDirective],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
