3ac32a3dfe3b8eb7381ef840fe09821f9b8da29c
Flavio Santos <flavio.a.dos.santos@avanade.com>
2025-04-15 11:55:49 -0300

    feat: Chamando servico dentro do compoente para criar novo produto

--- src/app/views/product/product.component.ts
+++ src/app/views/product/product.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product',
-  imports: [],
+  imports: [ MatButtonModule],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
 export class ProductComponent {
-
-}
+  constructor(private router: Router) { }
+  
+  navigateToProductsCreate() {
+    this.router.navigate(['/products/createad']);
+  }
+}  
+  
+ 
