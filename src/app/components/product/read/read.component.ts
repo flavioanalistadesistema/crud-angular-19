d405f7859e2b1dd3d0efdbff0f7d2e9be0293fc1
Flavio Santos <flavio.a.dos.santos@avanade.com>
2025-04-17 20:56:52 -0300

    feat: Collum Actions

--- src/app/components/product/read/read.component.ts
+++ src/app/components/product/read/read.component.ts
@@ -4,18 +4,19 @@ import { Product } from '../product.module';
 import { CommonModule, registerLocaleData } from '@angular/common';
 import { MatTableModule } from '@angular/material/table';
 import localePt from '@angular/common/locales/pt';
+import { RouterLink, RouterModule } from '@angular/router';
 
 registerLocaleData(localePt, 'pt-BR');
 
 @Component({
   selector: 'app-read',
-  imports: [CommonModule, MatTableModule],
+  imports: [CommonModule, MatTableModule, RouterModule, RouterLink],
   templateUrl: './read.component.html',
   styleUrl: './read.component.css'
 })
 export class ReadComponent implements OnInit {
   products: Product[] = [];
-  displayedColumns = ['id', 'name', 'price'];
+  displayedColumns = ['id', 'name', 'price', 'action'];
 
   constructor(private productService: ProductsService) { 
   }
