3ad060f9c4103755c32dcd0f1f1c2fe3c46947e8
Flavio Santos <flavio.a.dos.santos@avanade.com>
2025-04-17 20:45:55 -0300

    feat: tabela de produtos

--- src/app/components/product/read/read.component.ts
+++ src/app/components/product/read/read.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductsService } from '../products.service';
 import { Product } from '../product.module';
+import { CommonModule, registerLocaleData } from '@angular/common';
+import { MatTableModule } from '@angular/material/table';
+import localePt from '@angular/common/locales/pt';
+
+registerLocaleData(localePt, 'pt-BR');
 
 @Component({
   selector: 'app-read',
-  imports: [],
+  imports: [CommonModule, MatTableModule],
   templateUrl: './read.component.html',
   styleUrl: './read.component.css'
 })
 export class ReadComponent implements OnInit {
   products: Product[] = [];
+  displayedColumns = ['id', 'name', 'price'];
 
   constructor(private productService: ProductsService) { 
   }
@@ -17,8 +23,8 @@ export class ReadComponent implements OnInit {
   ngOnInit(): void {
     this.productService.read().subscribe((products) => {
       this.products = products;
+      console.log(this.products, 'products');
     });
     
-    console.log(this.products, 'products');
   }
 }
