0fdf0acc636e72c154cc5d47c06bfc5e43c1070f
Flavio Santos <flavio.a.dos.santos@avanade.com>
2025-04-17 21:21:01 -0300

    feat: Componente Update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
 import { ProductComponent } from './views/product/product.component';
 import { ProductCreateComponent } from './components/product/product-create/product-create.component';
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
 
 export const routes: Routes = [    
 { path: '', component: HomeComponent },
 { path: 'products', component: ProductComponent },
 { path: 'products/createad', component: ProductCreateComponent },
+{ path: 'products/update/:id', component: ProductUpdateComponent },
 { path: '**', redirectTo: '' } 
 ];
